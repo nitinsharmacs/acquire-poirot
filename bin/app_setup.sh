9dd3aaa8c1d773a03185c6875ff95caf8edb0b8f
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2022-08-06 10:04:42 +0530

    [#2] Nitin - App and Development setup
    * dev setup installs vscode extensions

---
+++ bin/app_setup.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+echo "Setting up the project"
+echo "Creating data directory"
+
+mkdir data 2> /dev/null
--- bin/dev_setup.sh
+++ bin/dev_setup.sh
@@ -1,12 +1,33 @@
 #! /bin/bash
 
-echo "Setting up the project"
-echo "Creating data directory"
-
-mkdir data 2> /dev/null
+GREEN="\x1b[32m"
+RED="\x1b[31m"
+RESET="\x1b[0m"
 
 echo "Configuring git"
 git config --local commit.template .github/commit_template
 git config --local core.hookspath .github/hookspath
+echo -e "Configured git\n"
 
-echo "Done"
+echo "Installing vscode extensions"
+declare -a extensions
+
+extensions=(
+  patbenatar.advanced-new-file
+  dbaeumer.vscode-eslint
+  bierner.markdown-checkbox
+  2gua.rainbow-brackets
+  vscode-icons-team.vscode-icons
+)
+
+for extension in ${extensions[@]}
+do
+  code --install-extension ${extension} &> /dev/null
+  if [[ $? != 0 ]]; then
+    echo -e "${RED}Error : Can't install ${extension}${RESET}"
+  else
+    echo -e "${GREEN}-> Installed ${extension}${RESET}"
+  fi
+done
+
+echo -e "\nDone"
