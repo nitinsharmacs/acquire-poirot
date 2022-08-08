2dc5a4b00b1a5f56c91a8fbc22c6ca1d9bb31f3d
Nilam Jadhav <99525537+nilamsjadhav@users.noreply.github.com>
2022-08-08 12:40:56 +0530

    [#12] ritika/nilam - Login
    Created auth router and served GET /login

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const { createApp } = require('./src/app.js');
+const fs = require('fs');
+require('dotenv').config();
 
-const config = { root: './public' };
-const app = createApp(config);
+const cookieName = process.env.COOKIE_NAME;
+const sessionKey = process.env.SESSION_KEY;
+const loginTemplatePath = process.env.LOGIN_TEMPLATE;
+
+const config = {
+  root: './public',
+  cookieConfig: { cookieName, sessionKey },
+  resources: { loginTemplatePath }
+};
+
+const app = createApp(config, fs);
 
 app.listen(8888, () => console.log('Listening on http://localhost:8888'));
