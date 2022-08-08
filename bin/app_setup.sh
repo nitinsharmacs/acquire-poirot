f6d4adff7d012b5d9e583376db70f3be0c7d36ad
Sampriti Das <99536474+sampritidas@users.noreply.github.com>
2022-08-08 14:58:16 +0530

    [#10] Sampriti/Tanmay - Add Host template path in app_setup.sh
    Added Host template path in app_setup.sh

--- bin/app_setup.sh
+++ bin/app_setup.sh
@@ -6,6 +6,7 @@ mkdir data 2> /dev/null
 
 echo -e "COOKIE_NAME = \"sessionId\"
 SESSION_KEY = \"hello\"
+HOST_TEMPLATE_PATH = \"resources/host-page.html\"
 LOGIN_TEMPLATE = \"resources/login.html\"" > .env
 
 echo "Installing dependencies"
