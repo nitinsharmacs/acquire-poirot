a30378de5c4e8f069e30c5ef284d1836482b6373
Nilam Jadhav <99525537+nilamsjadhav@users.noreply.github.com>
2022-08-08 12:53:07 +0530

    [#12] ritika/nilam - Login
    Added .env configuration in app_setup.sh

--- bin/app_setup.sh
+++ bin/app_setup.sh
@@ -4,5 +4,9 @@ echo "Setting up the project"
 echo -e "Creating data directory\n"
 mkdir data 2> /dev/null
 
+echo -e "COOKIE_NAME = \"sessionId\"
+SESSION_KEY = \"hello\"
+LOGIN_TEMPLATE = \"resources/login.html\"" > .env
+
 echo "Installing dependencies"
 npm install
