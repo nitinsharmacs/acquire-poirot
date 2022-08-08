b59e35fcd7e35f1fa3d4176c06b059116e93fb0c
Sampriti Das <99536474+sampritidas@users.noreply.github.com>
2022-08-08 15:52:34 +0530

    Added test cases for create game

--- test/testIndex.js
+++ test/testIndex.js
@@ -1,13 +1,35 @@
 const request = require('supertest');
 const { createApp } = require('../src/app.js');
 
-const config = { root: './public' };
+const fs = require('fs');
+
+const loginTemplatePath = 'resources/login.html';
+const hostPage = fs.readFileSync('resources/host-page.html', 'utf8');
+
+const config = {
+  root: './public',
+  cookieConfig: {
+    'COOKIE_NAME': 'sessionId',
+    'COOKIE_KEY': 'hello'
+  },
+  resources: { loginTemplatePath },
+  templates: { hostPage }
+};
 
 describe('GET /', () => {
   it('should show landing page', (done) => {
-    request(createApp(config))
+    request(createApp(config, fs))
       .get('/')
       .expect('Content-type', /html/)
       .expect(200, done);
+  });
+});
+
+describe('GET /create-game', () => {
+  it('should redirect to login page if session not present', (done) => {
+    request(createApp(config, fs))
+      .get('/create-game')
+      .expect('location', '/login')
+      .expect(302, done);
   });
 });
