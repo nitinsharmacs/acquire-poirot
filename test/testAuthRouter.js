8a610fc518e02b8f97d9573818ee91057847bb16
Nilam Jadhav <99525537+nilamsjadhav@users.noreply.github.com>
2022-08-09 12:18:36 +0530

    [#13] ritika/nilam - Added validation tests for GET /sign-up and POST /sign-up

--- test/testAuthRouter.js
+++ test/testAuthRouter.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const { createApp } = require('../src/app.js');
+const fs = require('fs');
 
 describe('GET /login', () => {
   const appConfig = {
@@ -44,6 +45,16 @@ describe('GET /login', () => {
 });
 
 describe('POST /login', () => {
+  beforeEach(done => {
+    const users = {
+      raju:
+        { username: 'raju', password: 'abc', id: '127824693' }
+    };
+
+    fs.writeFileSync('./test/testData/users.json',
+      JSON.stringify(users), 'utf8');
+    done();
+  });
   const appConfig = {
     root: './public',
     cookieConfig: {
@@ -77,6 +88,91 @@ describe('POST /login', () => {
     const req = request(createApp(appConfig));
     req.post('/login?ref=/join/1')
       .send('username=raju&password=abc')
+      .expect('location', '/join/1')
+      .expect(302, done);
+  });
+});
+
+describe('GET /sign-up', () => {
+  const appConfig = {
+    root: './public',
+    cookieConfig: {
+      sessionKey: 'hello'
+    },
+    resources: {
+      loginTemplatePath: './resources/login.html',
+      hostTemplatePath: './resources/host-page.html',
+      signupTemplatePath: './resources/sign-up.html'
+    },
+    db: { usersdbPath: './test/testData/users.json' }
+  };
+
+  it('should serve signup page', (done) => {
+    const req = request(createApp(appConfig));
+    req.get('/sign-up')
+      .expect('content-type', /html/)
+      .expect(200, done);
+  });
+
+  it('should serve login page with apt error message 409(user already exists)',
+    (done) => {
+      const req = request(createApp(appConfig));
+      req.get('/sign-up')
+        .set('Cookie', 'errCode=409')
+        .expect('content-type', /html/)
+        .expect(200, /User already exists/, done);
+    });
+
+  it('should serve login page with apt error msg 401(fields cannot be empty)',
+    (done) => {
+      const req = request(createApp(appConfig));
+      req.get('/sign-up')
+        .set('Cookie', 'errCode=401')
+        .expect('content-type', /html/)
+        .expect(200, /Fields cannot be empty/, done);
+    });
+});
+
+describe('POST /sign-up', () => {
+  beforeEach(done => {
+    fs.writeFileSync('./test/testData/users.json',
+      JSON.stringify({}), 'utf8');
+    done();
+  });
+
+  const appConfig = {
+    root: './public',
+    cookieConfig: {
+      sessionKey: 'hello'
+    },
+    resources: {
+      loginTemplatePath: './resources/login.html',
+      hostTemplatePath: './resources/host-page.html',
+      signupTemplatePath: './resources/sign-up.html'
+    },
+    db: { usersdbPath: './test/testData/users.json' }
+  };
+
+  it('should register and log the user in and set cookie', (done) => {
+    const req = request(createApp(appConfig));
+    req.post('/sign-up')
+      .send('username=rani&password=abc123')
+      .expect('location', '/')
+      .expect(302, done);
+  });
+
+  it('should redirect to /host', (done) => {
+    const req = request(createApp(appConfig));
+    req.post('/sign-up?ref=/host')
+      .send('username=raju&password=abc')
+      .expect('location', '/host')
+      .expect(302, done);
+  });
+
+  it('should redirect to /join/1', (done) => {
+    const req = request(createApp(appConfig));
+    req.post('/sign-up?ref=/join/1')
+      .send('username=diamond&password=die')
       .expect('location', '/join/1')
       .expect(302, done);
   });
