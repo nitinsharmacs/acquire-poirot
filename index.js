1c302a0d90c8e30cabc0e44bce773d2b617054bd
Addhanki Manikanta <99521159+maniaddhanki@users.noreply.github.com>
2022-08-17 14:03:38 +0530

    [#59] Manikanta/Geetanjali - Deploy to heroku
    * Extracted the port number into a variable

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { createApp } = require('./src/app.js');
 const app = createApp();
+const port = process.env.PORT || 8888;
 
-app.listen(8888, () => console.log('Listening on http://localhost:8888'));
+app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
