eda3a09bd6981c02e8f57b5e48844089b1786e11
Ritika Bhosale <54773240+ritikabhosale@users.noreply.github.com>
2022-08-23 20:50:29 +0530

    [#55] Ritika/Nitin - Refactored css of `auth`, `landing` and 'lobby' page
    * Removed login.css and added landingPage.css

--- src/views/index.js
+++ src/views/index.js
@@ -2,60 +2,34 @@ const landingPage = (username) => `<html>
 
 <head>
   <title>Acquire</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Recursive:regular,bold,bolditalic">
   <link rel="stylesheet" href="/css/index.css">
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css?family=Recursive:regular,bold,bolditalic">
+  <link rel="stylesheet" href="/css/landingPage.css">
 </head>
 
 <body>
 
-  <div class="main">
-    <div class="header">
-      <div class="heading">
+  <div class="page">
+    <header class="landing-page-header">
+      <div class="logo">
         Acquire
       </div>
       <div class="adv">plan, build and own the next super city</div>
       <div class="username">
         Welcome, ${username} !
       </div>
+    </header>
 
+    <div class="landing-options">
+      <a href="/host" class="btn">Create game</a>
+      <a href="/guide-page.html" class="btn">How to play</a>
     </div>
 
-    <div class="option-block">
-
-      <div class="avatar">
-        <img src="/images/hand_share.jpeg" alt="hand-share"
-          class="acquire-avatar">
-      </div>
-
-      <div class="options">
-        <div class="option">
-          <a href="/host">Create game</a>
-        </div>
-        <div class="option">
-          <a href="/guide-page.html">How to play</a>
-        </div>
-      </div>
-
-    </div>
-
-    <div class="footer-lane">
-      <div class="buildings"><img src="/images/buildingRow.jpeg"
-          alt="buildings">
-      </div>
-      <div class="buildings"><img src="/images/buildingRow.jpeg"
-          alt="buildings">
-      </div>
-      <div class="buildings"><img src="/images/buildingRow.jpeg"
-          alt="buildings">
-      </div>
-    </div>
   </div>
-
 </body>
 
 </html>`;
 
 module.exports = {
-  landingPage
+landingPage
 };
