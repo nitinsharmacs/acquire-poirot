091c6094e9d7192c63d127dfa31cceaa475a70fd
Nilam Jadhav <99525537+nilamsjadhav@users.noreply.github.com>
2022-08-26 17:49:04 +0530

    [#86] Nilam/Geetanjali - Create game not found page

--- src/handlers/notFound.js
+++ src/handlers/notFound.js
@@ -1,7 +1,7 @@
 const notFound = (req, res) => {
   res.status(404);
   res.type('text/html');
-  res.render('notFound', {});
+  res.render('notFound', { isNotFoundPage: true });
 };
 
 module.exports = { notFound };
