4786e0953aae96520ce2d47d548bbcc0352d1e60
Ritika Bhosale <54773240+ritikabhosale@users.noreply.github.com>
2022-08-26 12:55:07 +0530

    [#68] Nitin/Ritika - Create defunct stocks transaction model

--- public/js/renderer.js
+++ public/js/renderer.js
@@ -8,12 +8,16 @@ const renderLogs = ({ logs }) => {
 };
 
 const sellDefunctStocks = () => {
-  const stockCount = select('.sell-stocks').value;
-  sellStocks(stockCount);
+  const transactionFormElement = select('#transaction-form');
+  const form = new FormData(transactionFormElement);
+  const tradeCount = form.get('tradeCount');
+  const stockCount = form.get('stockCount');
+
+  return sellStocks({ stockCount, tradeCount });
 };
 
 const removeTransationPanel = () => {
-  const tradePanel = select('.trade');
+  const tradePanel = select('.transaction-panel');
   if (!tradePanel) {
     return;
   }
@@ -22,13 +26,36 @@ const removeTransationPanel = () => {
   playerActiviets.removeChild(tradePanel);
 };
 
+const createTransactionPanel = (message) => {
+  const panel = ['div', { class: 'transaction-panel' }, {},
+    ['form', { id: 'transaction-form', }, {},
+      ['h3', { class: 'component-heading' }, {}, 'Make Transaction'],
+      ['div', { class: 'panel-section' }, {},
+        ['label', { for: 'stockCount' }, {}, 'Sell'],
+        ['input', { type: 'number', name: 'stockCount', id: 'stockCount', class: 'stock-value', min: 0, value: 0 }]
+      ],
+      ['div', { class: 'panel-section' }, {},
+        ['label', { for: 'tradeCount' }, {}, 'Trade'],
+        ['input', { type: 'number', name: 'tradeCount', id: 'tradeCount', class: 'stock-value', step: 2, min: 0, value: 0 }]
+      ],
+      ['p', { class: 'stock-message' }, {}, message],
+      ['input', { type: 'button', class: 'btn theme-btn', value: 'Confirm' }, { onclick: sellDefunctStocks }]
+    ]
+  ];
+
+  return createDOMTree(panel);
+};
+
 const showDefunctStocksTransaction = (message = '') => {
   removeTransationPanel();
   const playerActivities = select('.player-activities');
-  const transactionHTML = [['div', { class: 'trade' }, {},
-    ['input', { class: 'sell-stocks' }, {}, ''], ['input', { type: 'button', value: 'confirm' }, { onclick: sellDefunctStocks }], ['p', {}, {}, message]]];
+  // const transactionHTML = [['div', { class: 'trade' }, {},
+  //   ['input', { class: 'sell-stocks' }, {}, ''], ['input', { type: 'button', value: 'confirm' }, { onclick: sellDefunctStocks }], ['p', {}, {}, message]]];
 
-  playerActivities.appendChild(...createElements(transactionHTML));
+  // playerActivities.appendChild(...createElements(transactionHTML));
+  const panel = createTransactionPanel(message);
+  playerActivities.appendChild(panel);
+  highlight(panel);
 };
 
 const renderScreen = (game) => {
