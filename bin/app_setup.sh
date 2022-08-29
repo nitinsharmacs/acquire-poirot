3e4cf912adac7d426a0da880ffce687b67887774
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2022-08-29 10:09:00 +0530

    [#81] Nitin/Sampriti - fix: app setup script missing quote

--- bin/app_setup.sh
+++ bin/app_setup.sh
@@ -1,13 +1,7 @@
 #! /bin/bash
 
 echo "Setting up the project"
-echo -e "Creating data directory\n"
-mkdir data 2> /dev/null
-echo {} > ./data/users.json
-echo [] > ./data/game.json
-echo [] > ./data/gameEntries.json
-
-echo -e "SESSION_KEY = \"hello\"
+echo -e "SESSION_KEY = \"hello\"" > .env
 
 echo "Installing dependencies"
 npm install
